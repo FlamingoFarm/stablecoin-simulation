584fbf72e1aaebd03a3204fe53dc72f2186a0580
Thomas Gehrmann <thomas.gehrmann@sva.de>
2024-03-05 15:34:56 +0100

    Calculate initial state

--- model/state_variables.py
+++ model/state_variables.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
 from enum import Enum
 from typing import List
-
-INITIAL_COLL_PRICE = 3000.0
+import numpy as np
 
 @dataclass
 class CollateralState:
-    collateral_price: float = INITIAL_COLL_PRICE
+    collateral_price: float
 
 @dataclass
 class StabilityPoolState:
@@ -43,3 +42,29 @@ class OwnerState:
 class AllOwnersState:
     owner: List[OwnerState]
 
+
+def set_initial_state(initial_coll_price, num_owners):
+    collateral = CollateralState(initial_coll_price)
+
+    vault_coll_balance = 20 + 400 * np.random.random(num_owners)
+    debt = 0.1 + 0.5 * np.random.random(num_owners) * initial_coll_price * vault_coll_balance
+    vault_states = [VaultState(*values) for values in zip(vault_coll_balance, debt)]
+
+    wallet_coll_balance = 5 + 10 * np.random.random(num_owners)
+    wallet_stable_coin_balance = 0.1 + 0.4 * np.random.random(num_owners) * debt
+    wallet_states = [WalletState(*values) for values in zip(wallet_coll_balance, wallet_stable_coin_balance)]
+
+    strategies = np.random.choice(OwnerStrategy, num_owners)
+
+    owners = [OwnerState(*values) for values in zip(vault_states, wallet_states, strategies)]
+
+    stability_pool_balance =np.sum(debt - wallet_stable_coin_balance)
+    stability_pool = StabilityPoolState(stability_pool_balance)
+        
+    initial_state = {
+        "colleteral": collateral,
+        "owners": owners,
+        "stability_pool": stability_pool
+    }
+
+    return initial_state
