b2b350e42a2d273df3d0d8035250822acc9fbff3
Thomas Gehrmann <thomas.gehrmann@sva.de>
2024-03-05 16:41:54 +0100

    Remove 'state' in name of state variables

--- model/state_variables.py
+++ model/state_variables.py
@@ -4,27 +4,29 @@ from typing import List
 import numpy as np
 
 @dataclass
-class CollateralState:
+class Collateral:
     collateral_price: float
 
 @dataclass
-class StabilityPoolState:
+class StabilityPool:
     stable_coin_balance: float
 
 @dataclass
-class VaultState:
+class Vault:
     collateral_balance: float
-    debt: float
-    liquidated: bool = False
+    debt_balance: float
+    # blocked becomes True if liquidation ratio is crossed for the first time 
+    # that means either the vault is fully liquidated or debt has not yet been fully paid due 
+    # to insufficient funds in the stability pool
+    blocked: bool = False
 
 @dataclass
-class WalletState:
+class Wallet:
     collateral_balance: float
     stable_coin_balance: float
 
 OwnerStrategy = Enum('OwnerStrategy', [
     'ACTIVE', # characterized by maintaining an appropriate liquidation buffer
-    'SENTIMENT_DRIVEN', # following the market sentiment
     'PASSIVE', # hands-off approach
     'RANDOM', # characterized by updating the vault randomly
     'SIMPLE_COLL', # maintains liquidation buffer via simple collateral adjustment
@@ -33,33 +35,33 @@ OwnerStrategy = Enum('OwnerStrategy', [
 ])
    
 @dataclass
-class OwnerState:
-    vault: VaultState
-    wallet: WalletState
+class Owner:
+    vault: Vault
+    wallet: Wallet
     strategy: OwnerStrategy
 
 @dataclass
-class AllOwnersState:
-    owner: List[OwnerState]
+class Owners:
+    owner: List[Owner]
 
 
-def set_initial_state(initial_coll_price, num_owners):
-    collateral = CollateralState(initial_coll_price)
+def set_initial_state(initial_coll_price: float, num_owners: int, liquidation_ratio: float):
+    collateral = Collateral(initial_coll_price)
 
     vault_coll_balance = 20 + 400 * np.random.random(num_owners)
-    debt = 0.1 + 0.5 * np.random.random(num_owners) * initial_coll_price * vault_coll_balance
-    vault_states = [VaultState(*values) for values in zip(vault_coll_balance, debt)]
+    debt = 0.1 + 0.8 * np.random.random(num_owners) * liquidation_ratio * initial_coll_price * vault_coll_balance
+    vault_states = [Vault(*values) for values in zip(vault_coll_balance, debt)]
 
     wallet_coll_balance = 5 + 10 * np.random.random(num_owners)
     wallet_stable_coin_balance = 0.1 + 0.4 * np.random.random(num_owners) * debt
-    wallet_states = [WalletState(*values) for values in zip(wallet_coll_balance, wallet_stable_coin_balance)]
+    wallet_states = [Wallet(*values) for values in zip(wallet_coll_balance, wallet_stable_coin_balance)]
 
     strategies = np.random.choice(OwnerStrategy, num_owners)
 
-    owners = [OwnerState(*values) for values in zip(vault_states, wallet_states, strategies)]
+    owners = [Owner(*values) for values in zip(vault_states, wallet_states, strategies)]
 
     stability_pool_balance =np.sum(debt - wallet_stable_coin_balance)
-    stability_pool = StabilityPoolState(stability_pool_balance)
+    stability_pool = StabilityPool(stability_pool_balance)
         
     initial_state = {
         "colleteral": collateral,
